1bdf21699dd1f8a525fc54d25cf62cc12de4860c
tynakpinska <m.k.kepinska@gmail.com>
2023-07-23 12:14:23 +0200

    add putting sign on the board, add toggling current player, block occupied boxes

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
-const gameboard = document.querySelector(".gameboard");
-
-const Gameboard = (() => {
-  const gameboard = ["x", "x", "x", "x", "x", "x", "x", "x", "x"];
-  const players = ["player1", "player2"];
-  const currentPlayer = players[0];
-  const toggleCurrentPlayer = () => {
-    currentPlayer === "player1" ? "player2" : "player1";
-  };
-  const resetGameboard = () => {};
-
-  return { gameboard, currentPlayer, toggleCurrentPlayer, resetGameboard };
-})();
+const gameboardElement = document.querySelector(".gameboard");
+const currentPlayerElement = document.querySelector(".currentPlayer");
 
 const Player = (name, sign) => {
-  const play = () => {};
+  const play = id => Gameboard.updateGameboard(id, sign);
   return { name, sign, play };
 };
 
 const player1 = Player("Tyna", "X");
 const player2 = Player("Rek", "O");
 
-Gameboard.gameboard.forEach((box, id) => {
-  const boxElement = document.createElement("div");
-  boxElement.innerHTML = box;
-  boxElement.className = "box";
-  boxElement.id = id;
-  gameboard.append(boxElement);
-});
+const Gameboard = (() => {
+  const gameboard = ["", "", "", "", "", "", "", "", ""];
+  let currentPlayer = player1;
+  currentPlayerElement.innerHTML = `${currentPlayer.name} - ${currentPlayer.sign}`;
+  const buildGameboard = () => {
+    gameboard.forEach((box, id) => {
+      const boxElement = document.createElement("div");
+      boxElement.innerHTML = box;
+      boxElement.className = "box";
+      boxElement.id = id;
+      gameboardElement.append(boxElement);
+      if (box === "") {
+        boxElement.addEventListener("click", () => currentPlayer.play(id));
+      }
+    });
+  };
+  const toggleCurrentPlayer = () => {
+    currentPlayer = currentPlayer === player1 ? player2 : player1;
+    currentPlayerElement.innerHTML = `${currentPlayer.name} - ${currentPlayer.sign}`;
+  };
+  const updateGameboard = (id, sign) => {
+    gameboard[id] = sign;
+    resetGameboard();
+    buildGameboard();
+    toggleCurrentPlayer();
+  };
+  const resetGameboard = () => (gameboardElement.innerHTML = "");
+  buildGameboard();
+
+  return {
+    updateGameboard,
+  };
+})();
