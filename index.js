ebe53ed1e5e0aaba4971dc562b9876c5ef12fdd1
tynakpinska <m.k.kepinska@gmail.com>
2023-08-15 19:01:39 +0200

    add determining winner and displaying result

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const gameboardElement = document.querySelector(".gameboard");
-const currentPlayerElement = document.querySelector(".currentPlayer");
+const playersElements = document.querySelectorAll(".player");
+const resultElement = document.querySelector(".result");
 const resetButtonElement = document.querySelector(".resetButton");
 
 const Player = (name, sign) => {
@@ -10,10 +11,14 @@ const Player = (name, sign) => {
 const player1 = Player("Tyna", "X");
 const player2 = Player("Rek", "O");
 
+playersElements[0].innerHTML = `${player1.name} - ${player1.sign}`;
+playersElements[1].innerHTML = `${player2.name} - ${player2.sign}`;
+
 const Gameboard = (() => {
   let gameboard = ["", "", "", "", "", "", "", "", ""];
   let currentPlayer = player1;
-  currentPlayerElement.innerHTML = `${currentPlayer.name} - ${currentPlayer.sign}`;
+  playersElements[0].classList.add("current");
+
   const buildGameboard = () => {
     gameboard.forEach((box, id) => {
       const boxElement = document.createElement("div");
@@ -26,19 +31,50 @@ const Gameboard = (() => {
       }
     });
   };
+
+  const checkIfWin = () => {
+    if (
+      (gameboard[0] === gameboard[1] &&
+        gameboard[1] === gameboard[2] &&
+        gameboard[2] !== "") ||
+      (gameboard[3] === gameboard[4] &&
+        gameboard[4] === gameboard[5] &&
+        gameboard[5] !== "") ||
+      (gameboard[6] === gameboard[7] &&
+        gameboard[7] === gameboard[8] &&
+        gameboard[8] !== "") ||
+      (gameboard[0] === gameboard[3] &&
+        gameboard[3] === gameboard[6] &&
+        gameboard[6] !== "") ||
+      (gameboard[1] === gameboard[4] &&
+        gameboard[4] === gameboard[7] &&
+        gameboard[7] !== "") ||
+      (gameboard[2] === gameboard[5] &&
+        gameboard[5] === gameboard[8] &&
+        gameboard[8] !== "") ||
+      (gameboard[0] === gameboard[4] &&
+        gameboard[4] === gameboard[8] &&
+        gameboard[8] !== "") ||
+      (gameboard[2] === gameboard[4] &&
+        gameboard[4] === gameboard[6] &&
+        gameboard[6] !== "")
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
   const toggleCurrentPlayer = () => {
     currentPlayer = currentPlayer === player1 ? player2 : player1;
-    currentPlayerElement.innerHTML = `${currentPlayer.name} - ${currentPlayer.sign}`;
+    playersElements[0].classList.toggle("current");
+    playersElements[1].classList.toggle("current");
   };
-  const updateGameboard = (id, sign) => {
-    gameboard[id] = sign;
-    deleteGameboard();
-    buildGameboard();
-    toggleCurrentPlayer();
-  };
+
   const resetGameboard = () => {
     deleteGameboard();
     gameboard = ["", "", "", "", "", "", "", "", ""];
+    resultElement.innerHTML = "";
     buildGameboard();
   };
 
@@ -46,6 +82,14 @@ const Gameboard = (() => {
     gameboardElement.innerHTML = "";
   };
   buildGameboard();
+
+  const updateGameboard = (id, sign) => {
+    gameboard[id] = sign;
+    deleteGameboard();
+    buildGameboard();
+    const ifWin = checkIfWin();
+    ifWin ? (resultElement.innerHTML = `${sign} wins!`) : toggleCurrentPlayer();
+  };
 
   return {
     updateGameboard,
